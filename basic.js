48f8b4e623907bc4675b734b904a04a99f68a477
silpiAkter <silpiakter461@gmail.com>
2024-08-30 13:07:05 +0600

    added basic condition

---
+++ basic.js
@@ -0,0 +1,17 @@
+/***
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+var burger = 500;
+var coke = 30;
+
+if(burger >= 500){
+    console.log('coke free');
+}
+else{
+    console.log(coke);
+}
+
+
